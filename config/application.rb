2a4c1dc10621e99ae7b5c5b08af78ef2393e174a
Maxim <qew7777@gmail.com>
2020-04-16 22:28:13 +0300

    Add ru locales

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,10 @@ module RadApi
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    I18n.load_path += Dir[Rails.root.join('lib', 'locale', '*.{rb,yml}')]
+    I18n.available_locales = [:ru]
+    I18n.default_locale = :ru
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
